e5b784a6197f5a7c6060d85dc09d96902b1d9871
toto01tdma <toto01tdma@gmail.com>
2025-02-07 09:59:05 +0700

    first update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# flutter_word_guess_game
+# flutter_word_game_app
+
+This game is a project I created to develop my app development skills with Flutter. The game I made can also be used by children to enhance their vocabulary skills.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(WordGameApp());
+}
+
+class WordGameApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Word Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WordGameHomePage(),
+    );
+  }
+}
+
+class WordGameHomePage extends StatefulWidget {
+  @override
+  _WordGameHomePageState createState() => _WordGameHomePageState();
+}
+
+class _WordGameHomePageState extends State<WordGameHomePage> {
+  final List<String> words = ['APPLE', 'BALL', 'CAT', 'DOG', 'EGG'];
+  late String currentWord;
+  late List<String> shuffledWord;
+  late List<String> userAnswer;
+
+  @override
+  void initState() {
+    super.initState();
+    _generateNewWord();
+  }
+
+  void _generateNewWord() {
+    setState(() {
+      currentWord = words[Random().nextInt(words.length)];
+      shuffledWord = currentWord.split('')..shuffle();
+      userAnswer = [];
+    });
+  }
+
+  void _checkAnswer() {
+    if (userAnswer.join('') == currentWord) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Correct!'),
+          content: Text('You arranged the word correctly!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _generateNewWord();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Try again!')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Game'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Arrange the letters to form the word:',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(height: 20),
+          Wrap(
+            spacing: 10,
+            children: shuffledWord.map((letter) {
+              return Draggable<String>(
+                data: letter,
+                child: _buildLetterTile(letter),
+                feedback: _buildLetterTile(letter, dragging: true),
+                childWhenDragging: _buildLetterTile('', empty: true),
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 40),
+          Wrap(
+            spacing: 10,
+            children: List.generate(currentWord.length, (index) {
+              return DragTarget<String>(
+                onAccept: (receivedLetter) {
+                  setState(() {
+                    userAnswer.add(receivedLetter);
+                    shuffledWord.remove(receivedLetter);
+                  });
+                },
+                builder: (context, candidateData, rejectedData) {
+                  return _buildLetterTile(
+                    userAnswer.length > index ? userAnswer[index] : '',
+                    empty: userAnswer.length <= index,
+                  );
+                },
+              );
+            }),
+          ),
+          SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: _checkAnswer,
+            child: Text('Check Answer'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLetterTile(String letter,
+      {bool dragging = false, bool empty = false}) {
+    return Container(
+      width: 50,
+      height: 50,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: empty
+            ? Colors.grey[300]
+            : dragging
+                ? Colors.blue.withOpacity(0.7)
+                : Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        letter,
+        style: TextStyle(
+          fontSize: 24,
+          color: empty ? Colors.grey : Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
